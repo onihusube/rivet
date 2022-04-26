807313528edea035f5d7eb1f6eb084cbefe579a7
onihusube <44743040+onihusube@users.noreply.github.com>
2022-04-26 16:58:57 +0900

    GCC10/11まで

---
+++ include/rivet.hpp
@@ -0,0 +1,136 @@
+#pragma once
+
+#include <version>
+
+#if defined(__cpp_lib_ranges) && __cpp_lib_ranges < 202202L
+  #if defined(__GNUC__) && __GNUC__ == 11
+    #define RIVET_GCC11
+  #elif defined(__GNUC__) && __GNUC__ == 10
+    #define RIVET_GCC10
+  #elif defined(__clang__)
+    #define RIVET_CLANG
+  #elif defined(_MSC_VER)
+    #define RIVET_MSVC
+  #else
+    #error No support compiler
+  #endif
+#elif defined(__cpp_lib_ranges)
+  #define RIVET_P2387
+#else
+  #error This environment does not implement <ranges>
+#endif
+
+#include <ranges>
+#include <concepts>
+
+namespace rivet::detail {
+
+#ifndef RIVET_GCC10
+
+  template<typename Adoptor, int Arity = 2>
+  struct range_adaptor_base_impl
+#ifdef RIVET_GCC11
+    : public std::views::__adaptor::_RangeAdaptor<range_adaptor_base_impl<Adoptor>>
+#endif
+  {
+#ifdef RIVET_GCC11
+    using std::views::__adaptor::_RangeAdaptor<range_adaptor_base_impl<Adoptor>>::operator();
+
+    static constexpr int _S_arity = Arity;
+    static constexpr bool _S_has_simple_call_op = true;
+
+    template <std::ranges::viewable_range R, typename... Args>
+    constexpr auto operator()(R&& r, Args&&... args) const {
+      const auto& self = static_cast<const Adoptor&>(*this);
+      return self(std::forward<R>(r), std::forward<Args>(args)...);
+    }
+#else
+
+    template<typename Args>
+      requires std::constructible_from<std::decay_t<Args>, Args>
+    constexpr auto operator()(Args&& args) const noexcept {
+  #if defined(RIVET_CLANG)
+      return std::__range_adaptor_closure_t(std::__bind_back(*this, std::forward<Arg>(arg)));
+  #elif defined(RIVET_MSVC)
+      return std::ranges::_Range_closure<range_adaptor_base_impl<Adoptor, false>, std::decay_t<Arg>>{std::forward<Arg>(arg)};
+  #endif
+    }
+#endif
+  };
+
+#else
+
+  template<typename Adoptor, auto...>
+  struct range_adaptor_base_impl {
+
+    template <typename... Args>
+    constexpr auto operator()(Args&&... args) const {
+      if constexpr (std::default_initializable<Adoptor>) {
+        auto closure = [... args(std::forward<Args>(args))]<typename R>(R &&r) {
+          return Adoptor{}(std::forward<R>(r), args...);
+        };
+
+        return std::views::__adaptor::_RangeAdaptorClosure<decltype(closure)>(std::move(closure));
+      } else {
+        static_assert([]{return false;}(), "Adoptor must be default_initializable.");
+      }
+    }
+  };
+
+  template <typename Adoptor>
+  struct range_adoptor_closure_base {
+
+    // 1. range | RACO -> view
+    template <std::ranges::viewable_range R>
+    [[nodiscard]]
+    friend constexpr auto operator|(R &&r, const Adoptor& self) {
+      return self(std::forward<R>(r));
+    }
+
+    // 2. RACO | RACO -> RACO
+    template<typename LHS, typename RHS>
+    [[nodiscard]]
+    friend constexpr auto operator|(LHS lhs, RHS rhs) {
+      auto closure = [l = std::move(lhs), r = std::move(rhs)]<typename R>(R &&range) {
+        return std::forward<R>(range) | l | r;
+      };
+
+      return std::views::__adaptor::_RangeAdaptorClosure<decltype(closure)>(std::move(closure));
+    }
+  };
+
+#endif
+
+
+  template<typename Adoptor, auto...>
+  struct dispatcher {
+#ifdef RIVET_GCC11
+    using type = std::views::__adaptor::_RangeAdaptorClosure;
+#elif defined(RIVET_GCC10)
+    using type = range_adoptor_closure_base<Adoptor>;
+#elif defined(RIVET_CLANG)
+    using type = std::__range_adaptor_closure<Adoptor>;
+#elif defined(RIVET_MSVC)
+    using type = std::ranges::_Pipe::_Base<Adoptor>;
+#endif
+  };
+
+  template<typename Adoptor, int Arity>
+  struct dispatcher<Adoptor, false, Arity> {
+    using type = range_adaptor_base_impl<Adoptor, Arity>;
+  };
+}
+
+namespace rivet {
+
+  template <typename Adoptor, bool IsClosure = false, int Arity = 2>
+  using range_adaptor_base = typename detail::dispatcher<std::decay_t<Adoptor>, IsClosure, Arity>::type;
+}
+
+#define ENABLE_RANGE_ADOPTOR(this_type) using rivet::range_adaptor_base<this_type>::operator()
+
+#undef RIVET_GCC11
+#undef RIVET_GCC10
+#undef RIVET_CLANG
+#undef RIVET_MSVC
+#undef RIVET_P2387
---
+++ test/rivet_test.cpp
@@ -0,0 +1,129 @@
+#include <ranges>
+#include <algorithm>
+
+#include "rivet.hpp"
+
+#define BOOST_UT_DISABLE_MODULE
+#include <boost/ut.hpp>
+
+namespace ut = boost::ut;
+using namespace boost::ut::literals;
+using namespace boost::ut::operators::terse;
+
+namespace myranges::views {
+  namespace detail {
+    
+    struct filter_adoptor : public rivet::range_adaptor_base<filter_adoptor> {
+
+      template<std::ranges::viewable_range R, typename Pred>
+      constexpr auto operator()(R&& r, Pred&& p) const {
+        return std::ranges::filter_view(std::forward<R>(r), std::forward<Pred>(p));
+      }
+
+      //using rivet::range_adaptor_base<filter_adoptor>::operator();
+      ENABLE_RANGE_ADOPTOR(filter_adoptor);
+    };
+
+    struct common_adoptor_closure : public rivet::range_adaptor_base<common_adoptor_closure, true>
+    {
+      template<std::ranges::viewable_range R>
+      constexpr auto operator()(R&& r) const {
+          if constexpr (std::ranges::common_range<R>) {
+              return std::views::all(std::forward<R>(r));
+          } else {
+              return std::ranges::common_view(std::forward<R>(r));
+          }
+      }
+    };
+  }
+
+  inline constexpr detail::filter_adoptor filter;
+  inline constexpr detail::common_adoptor_closure common;
+}
+
+int main() {
+
+  "range adoptor partial"_test = []{
+    auto even = [](int n) { return n % 2 == 0;};
+
+    auto fil1 = myranges::views::filter(even);
+    auto fil2 = std::views::filter(even);
+
+    auto seq1 = fil1(std::views::iota(1));
+    auto seq2 = fil2(std::views::iota(1));
+
+    auto it1 = seq1.begin();
+    auto it2 = seq2.begin();
+    for ([[maybe_unused]] int i : {1, 2, 3}) {
+      ut::expect(*it1 == *it2);
+
+      ++it1;
+      ++it2;
+    }
+  };
+
+  "range adoptor invoke"_test = [] {
+    auto even = [](int n) { return n % 2 == 0;};
+    
+    auto seq1 = myranges::views::filter(std::views::iota(1), even);
+    auto seq2 = std::views::filter(std::views::iota(1), even);
+
+    auto it1 = seq1.begin();
+    auto it2 = seq2.begin();
+    for ([[maybe_unused]] int i : {1, 2, 3}) {
+      ut::expect(*it1 == *it2);
+
+      ++it1;
+      ++it2;
+    }
+  };
+
+  "range adoptor join"_test = [] {
+    auto even = [](int n) { return n % 2 == 0;};
+    
+    auto raco1 = myranges::views::filter(even) | myranges::views::common;
+    auto raco2 = std::views::filter(even) | std::views::common;
+
+    auto seq1 = std::views::iota(1) | raco1;
+    auto seq2 = std::views::iota(1) | raco2;
+
+    auto it1 = seq1.begin();
+    auto it2 = seq2.begin();
+    for ([[maybe_unused]] int i : {1, 2, 3}) {
+      ut::expect(*it1 == *it2);
+
+      ++it1;
+      ++it2;
+    }
+  };
+
+  "range adoptor join invoke"_test = [] {
+    auto even = [](int n) { return n % 2 == 0;};
+    
+    auto raco1 = myranges::views::filter(even) | myranges::views::common;
+    auto raco2 = std::views::filter(even) | std::views::common;
+
+    auto seq1 = raco1(std::views::iota(1));
+    auto seq2 = raco2(std::views::iota(1));
+
+    auto it1 = seq1.begin();
+    auto it2 = seq2.begin();
+    for ([[maybe_unused]] int i : {1, 2, 3}) {
+      ut::expect(*it1 == *it2);
+
+      ++it1;
+      ++it2;
+    }
+  };
+
+  "pipe"_test = [] {
+    auto seq1 = std::views::iota(1) | myranges::views::filter([](int n) { return n % 2 == 0;})
+                                    | myranges::views::common
+                                    | std::views::take(5);
+    auto seq2 = std::views::iota(1) | std::views::filter([](int n) { return n % 2 == 0;})
+                                    | std::views::common
+                                    | std::views::take(5);
+
+    ut::expect(std::ranges::equal(seq1, seq2));
+  };
+}
